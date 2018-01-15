2b129b240b2b26b2b08e475ce86eaad0a8f04e66
daizengji <daizengji@gzkit.com.cn>
2018-01-15 13:22:06 +0800

    feat: framework function

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MainRoutingModule } from './main-routing.module';
-import { MainPageComponent } from './main-page/main-page.component';
+import {MainRoutingModule} from './main-routing.module';
+import {MainPageComponent} from './main-page/main-page.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    NgZorroAntdModule
   ],
   declarations: [MainPageComponent]
 })
-export class MainModule { }
+export class MainModule {
+}
